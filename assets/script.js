0a8b285682910b801ad4d766daf53500a9f1c551
wavir <197363266+wavir@users.noreply.github.com>
2025-06-02 14:12:22 +0200

    Update script.js
    Signed-off-by: wavir <197363266+wavir@users.noreply.github.com>

--- assets/script.js
+++ assets/script.js
@@ -27,99 +27,64 @@ if (query != null) {
     // Search all entries
     for (var i = 0; i < dictionary.length; i++) {
         // Search lemma
-        if (dictionary[i].l == query) {
+        const lCheck = " " + dictionary[i].l + " ";
+        if (lCheck.includes(" " + query + " ") ||
+            lCheck.includes(" " + query + ",")) {
             matches.push(dictionary[i]);
             continue outer;
         }
-        // Search all variants for each entry
-        for (var j = 0; j < dictionary[i].v.length; j++) {
-            if (dictionary[i].v[j] == query) {
+        // Search all translations for each entry
+        for (var j = 0; j < dictionary[i].t.length; j++) {
+            // Search the English translations
+            const eCheck = " " + dictionary[i].t[j].e + " ";
+            if (eCheck.includes(" " + query + " ") ||
+                eCheck.includes(" " + query + ",")) {
+                matches.push(dictionary[i]);
+                continue outer;
+            }
+            // Search the German translation
+            const gCheck = " " + dictionary[i].t[j].g + " ";
+            if (gCheck.includes(" " + query + " ") ||
+                gCheck.includes(" " + query + ",")) {
                 matches.push(dictionary[i]);
                 continue outer;
             }
         }
-        // Search all translations for each entry
-        for (var j = 0; j < dictionary[i].t.length; j++) {
-            // Search all English translations 
-            for (var k = 0; k < dictionary[i].t[j].e.length; k++) {
-                if (dictionary[i].t[j].e[k] == query) {
-                    matches.push(dictionary[i]);
-                    continue outer;
-                }
-            }
-            // Search all German translations 
-            for (var k = 0; k < dictionary[i].t[j].g.length; k++) {
-                if (dictionary[i].t[j].g[k] == query) {
-                    matches.push(dictionary[i]);
-                    continue outer;
-                }
-            }
-        }
     }
-
-    content += "<span style=\"color:black\"><b>Community Dictionary of Latin Online</b><br><span class=\"fs-6\">The search for ‘" + query + "’ returned " + matches.length + " result(s).</span></span><br><br>";
 
     if (matches.length == 0) {
         document.title = "No result | Latinarium";
-        content += "<div class=\"fs-4\"><i class=\"bi bi-exclamation-diamond\"></i> There are no results</div>";
+        content += "<div class=\"fs-4\"><i class=\"bi bi-exclamation-diamond\"></i> No results 🢝 Keine Treffer</div>";
     } else {
+        content += "<span style=\"color:black\"><span class=\"fs-6\">" + matches.length + " result(s) • " + matches.length + " Treffer</span></span><br><br>";
         for (var i = 0; i < matches.length; i++) {
-            content += "<div class=\"result-div\">" + matches[i].l + " ";
-
-            if (matches[i].f != "") {
-                content += matches[i].f + " ";
-            }
-
+            //content += "<div class=\"result-div\"><span class=\"fw-bold\">" + matches[i].l + "</span>";
+            content += "<div class=\"card result-card\"><div class=\"card-body\"><span class=\"fw-bold\">" + matches[i].l + "</span>";
             if (matches[i].c != "") {
-                content += "[" + matches[i].c + "]<br>";
+                content += " <i>" + matches[i].c + "</i><br>";
             } else {
                 content += "<br>";
             }
 
-            if (matches[i].v.length > 0) {
-                content += "<br><b>Variants</b><br>"
-                for (var j = 0; j < matches[i].v.length; j++) {
-                    content += matches[i].v[j] + ", ";
-                }
-                content = content.substring(0, content.length - 2);
-                content += "<br>";
-            }
-
-            if (matches[i].n != "") {
-                content += "<br><b>Notes</b><br>" + matches[i].n + "<br>";
-            }
-
-            if (matches[i].e != "") {
-                content += "<br><b>Etymology</b><br>" + matches[i].e + "<br>";
-            }
-
             if (matches[i].t.length > 0) {
-                content += "<br><b>Translations</b><br><table class=\"table table-borderless result-table\">"
+                content += "<table class=\"table\"><tbody>"
                 for (var j = 0; j < matches[i].t.length; j++) {
-                    content += "<tr><td class=\"translation-td\">" + matches[i].t[j].e + "</td><td class=\"translation-td\">" + matches[i].t[j].g + "</td></tr>";
+                    content += "<tr><td class=\"ps-0\">" + matches[i].t[j].e + "</td><td class=\"pe-0\">" + matches[i].t[j].g + "<tr>";
                 }
-                content += "</table>";
+                content += "</tbody></table>"
+                //content = content.substring(0, content.length - 4);
             }
-
-            if (matches[i].d.length > 0) {
-                content += "<br><b>Derivatives</b><br>"
-                for (var j = 0; j < matches[i].d.length; j++) {
-                    content += matches[i].d[j] + ", ";
-                }
-                content = content.substring(0, content.length - 2);
-                content += "<br>";
-            }
-
             if (matches[i].r.length > 0) {
-                content += "<br><b>References</b><br>"
+                content += " "
                 for (var j = 0; j < matches[i].r.length; j++) {
-                    content += "<u class=\"references-u\"" + references[k].c + "\">" + matches[i].r[j] + "</u>  ";
+                    content += "<span class=\"references\">" + matches[i].r[j] + "</span>";
                 }
-                content = content.substring(0, content.length - 2);
+                content = content.substring(0, content.length - 3);
                 content += "<br>";
             }
-            
-            content += "</div><br>";
+
+            //content += "</div><br>";
+            content += "</div></div><br>";
         }
     }
     contentDiv.innerHTML = content;
@@ -129,7 +94,7 @@ if (query != null) {
 } else if (page == "references") {
     var content = "<table class=\"table table-hover table-borderless\">";
     for (var i = 0; i < references.length; i++) {
-        content += "<tr><td class=\"references-td\">" + references[i].r + "</td><td>" + references[i].c + "</td><td>"
+        content += "<tr><td>" + references[i].r + "</td><td>" + references[i].c + "</td><td>"
         if (references[i].l != "") {
             content += "<a target=\"_blank\" href=\"" + references[i].l + "\"><i class=\"bi bi-box-arrow-up-right\"></i></a>";
         }
